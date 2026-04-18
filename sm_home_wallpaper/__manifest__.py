3d0f3aee27330f80d90d45782d51f38194cdfc67
stevenmarp <stevenmarp@sinerka.id>
2026-04-18 17:36:22 +0700

    Backport to Odoo 18

--- sm_home_wallpaper/__manifest__.py
+++ sm_home_wallpaper/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Home Screen Wallpaper',
-    'version': '19.0.1.0.0',
+    'version': '18.0.1.0.0',
     'category': 'Extra Tools',
     'summary': 'Customize the Odoo home screen with random background wallpapers',
     'description': """
